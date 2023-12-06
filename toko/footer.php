356e3bf977dacd5bffc4df55252f8a309ab7ad73
Lepiii <113070715+Lepiii@users.noreply.github.com>
2023-12-06 17:00:25 +0700

    Add files via upload

---
+++ toko/footer.php
@@ -0,0 +1,24 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ff8680"><b>SEPA-CAKE BAKERY</b></h3>
+					<p>Jl. In Aja Dulu </p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6212345678901</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> sepaafham@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #ff8680; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Ale Pradita Rizal Irwanysah</span>
+		</div>
+	</footer>
+
+</body>
+</html>
